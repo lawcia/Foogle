420bd2e0fd06466c84fa85a2789808c71932be8e
lawcia <lawrencianjume@gmail.com>
2020-05-31 20:35:17 +0100

    should render auto complete options

--- project/frontend/src/components/Search/SearchLocationInput.js
+++ project/frontend/src/components/Search/SearchLocationInput.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-const SearchLocationInput = (props) => (
+const SearchLocationInput = () => (
   <div className="control has-icons-left stretch">
     <input
       className="input"
       type="text"
       name="searchLocation"
-      onChange={props.handleChangeLocation}
       placeholder="79 Borough Road"
     />
     <span className="icon is-small is-left">
       <i class="fas fa-map-marked-alt"></i>
     </span>
-    <p>{this.props.match}</p>
   </div>
 );
 
--- project/frontend/src/components/Search/SearchLocationInput.test.js
+++ project/frontend/src/components/Search/SearchLocationInput.test.js
@@ -4,13 +4,49 @@ import SearchLocationInput from "./SearchLocationInput";
 
 describe("<Search Location Input />", () => {
 
+  const features = [
+        {
+            "place_name": "London, Greater London, England, United Kingdom",
+            "center": [
+                -0.1275,
+                51.50722
+            ]
+        },
+        {
+            "place_name": "London, Ontario, Canada",
+            "center": [
+                -81.246,
+                42.9881
+            ]
+             },
+        {
+            "place_name": "Enfield, Greater London, England, United Kingdom",
+            "center": [
+                -0.06,
+                51.645
+            ],
+         },
+        {
+            "place_name": "Londonderry, Derry, Northern Ireland, United Kingdom",
+            "center": [
+                -7.34167,
+                54.99167
+            ],
+           }
+    ];
+
   let wrapper;
-  
+
   beforeEach(() => {
-    wrapper = shallow(<SearchLocationInput />)
+    wrapper = shallow(<SearchLocationInput options={features} />)
   })
 
   it("should render an text input", () => {
-    expect(wrapper.find(input).length).toEqual(1);
+    expect(wrapper.find("input[type='text']").length).toEqual(1);
+  })
+
+  it("should render 4 place name options in div", () => {
+    expect(wrapper.find("#place-options").length).toEqual(1);
+    expect(wrapper.find(".place-option").length).toEqual(4);
   })
 })
