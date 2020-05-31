b21a73bff949ae957a2043ba74a5c97154d753e1
lawcia <lawrencianjume@gmail.com>
2020-05-31 16:39:47 +0100

    provide db default

--- project/project/settings.py
+++ project/project/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
     }
 }
 
-if os.environ.get('ENV') == "PROD":
+if os.environ.get('ENV', '') == "PROD":
     import dj_database_url
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
