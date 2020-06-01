c77ae37626ba165ceb69d5c15da1fe282973ee70
lawcia <lawrencianjume@gmail.com>
2020-06-01 23:55:07 +0100

    wrote search reducer should add features to state test

--- project/frontend/src/redux/reducers/searchReducers.test.js
+++ project/frontend/src/redux/reducers/searchReducers.test.js
@@ -1,10 +1,16 @@
 import searchReducer from "./searchReducer";
 import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
+import { data } from "../../mockAPIs/mapbox";
 
 describe("search reducer", () => {
-  it("should render initial state", () => {
+  it("should return initial state", () => {
     expect(searchReducer(undefined, {}))
     .toEqual(initialState)
+  })
+
+  it("should return state with features data", () => {
+    expect(searchReducer(undefined, types.LOAD_FEATURES_SUCCESS))
+    .toEqual({...initialState, features: data.features})
   })
 })
