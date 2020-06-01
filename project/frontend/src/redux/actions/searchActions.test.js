4b2f970cc81b22d79c7fa110c9d77b142a94bee3
lawcia <lawrencianjume@gmail.com>
2020-06-01 02:50:30 +0100

    create tests for search actions

--- project/frontend/src/redux/actions/searchActions.test.js
+++ project/frontend/src/redux/actions/searchActions.test.js
@@ -1,52 +1,27 @@
 import * as searchActions from "./searchActions";
 import * as types from "./actionTypes";
-import { courses } from "../../../tools/mockData";
+import { data } from "../../mockAPIs/mapbox.data";
 import thunk from "redux-thunk";
-import fetchMock from "fetch-mock";
 import configureMockStore from "redux-mock-store";
+import axios from "axios";
 
-// Test an async action
+
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
+jest.mock("axios");
 
-describe("Async Actions", () => {
-  afterEach(() => {
-    fetchMock.restore();
-  });
+describe("Search actions", () => {
+  it("should create LOAD_FEATURES_SUCCESS when loading features", () => {
+    
+    axios.get.mockImplementation(() => Promise.resolve(data))
 
-  describe("Load Courses Thunk", () => {
-    it("should create BEGIN_API_CALL and LOAD_COURSES_SUCCESS when loading courses", () => {
-      fetchMock.mock("*", {
-        body: courses,
-        headers: { "content-type": "application/json" }
-      });
+    const expectedAction = [
+      { type: types.LOAD_FEATURES_SUCCESS, payload: data.features }
+    ];
 
-      const expectedActions = [
-        { type: types.BEGIN_API_CALL },
-        { type: types.LOAD_COURSES_SUCCESS, courses }
-      ];
-
-      const store = mockStore({ courses: [] });
-      return store.dispatch(courseActions.loadCourses()).then(() => {
-        expect(store.getActions()).toEqual(expectedActions);
-      });
+    const store = mockStore({ features: [] });
+    return store.dispatch(searchActions.loadFeatures("london")).then(() => {
+      expect(store.getActions()).toEqual(expectedAction);
     });
-  });
-});
-
-describe("createCourseSuccess", () => {
-  it("should create a CREATE_COURSE_SUCCESS action", () => {
-    //arrange
-    const course = courses[0];
-    const expectedAction = {
-      type: types.CREATE_COURSE_SUCCESS,
-      course
-    };
-
-    //act
-    const action = courseActions.createCourseSuccess(course);
-
-    //assert
-    expect(action).toEqual(expectedAction);
   });
 });
