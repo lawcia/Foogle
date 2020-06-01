2db141e8a08d9c82003e9eea47cef94317585f3d
lawcia <lawrencianjume@gmail.com>
2020-06-01 12:44:50 +0100

    api call error action  test

--- project/frontend/src/api/mapBoxApi.js
+++ project/frontend/src/api/mapBoxApi.js
@@ -4,12 +4,12 @@ import axios from "axios";
 const token = process.env.REACT_APP_MAPBOX_TOKEN;
 
 export function convertSearchLocationToCoordinates(searchLocation) {
-  return axios
-    .get(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchLocation}.json?access_token=${token}`
-    ).then(async (response) => {
-      console.log(response)
-      if (response.status === 200) return response;
-      throw new Error();
-    }).catch(error => error)
+  return axios.get(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchLocation}.json?access_token=${token}`
+  ).then(response => {
+    if (response.status === 200) return response.data.features;
+    throw new Error();
+  }).catch(error => {
+    throw error;
+  })
 }
