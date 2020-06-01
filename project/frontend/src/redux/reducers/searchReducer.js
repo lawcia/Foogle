611efa4068a86be2417dd4159121a3e29c168c41
lawcia <lawrencianjume@gmail.com>
2020-06-01 03:50:04 +0100

    set feature state

--- project/frontend/src/redux/reducers/searchReducer.js
+++ project/frontend/src/redux/reducers/searchReducer.js
@@ -1,6 +1,13 @@
 import initialState from "./initialState";
+import * as types from "../actions/actionTypes";
 
-
-export default function searchReducer(state=initialState, action ){
-  return state
+export default function searchReducer(state = initialState, action) {
+  switch (action.type) {
+    case types.LOAD_FEATURES_SUCCESS:
+      return {
+        ...state, features: action.payload
+      }
+    default:
+      return state;
+  }
 }
