7b3fecaf8a26fbd33bd8206932a80380485574d0
lawcia <lawrencianjume@gmail.com>
2020-06-01 02:03:07 +0100

    create mock data file

---
+++ project/frontend/src/__mocks__/mapbox.data.js
@@ -0,0 +1,33 @@
+module.exports = {
+  data: [{
+    features: [{
+        "place_name": "London, Greater London, England, United Kingdom",
+        "center": [
+          -0.1275,
+          51.50722
+        ]
+      },
+      {
+        "place_name": "London, Ontario, Canada",
+        "center": [
+          -81.246,
+          42.9881
+        ]
+      },
+      {
+        "place_name": "Enfield, Greater London, England, United Kingdom",
+        "center": [
+          -0.06,
+          51.645
+        ],
+      },
+      {
+        "place_name": "Londonderry, Derry, Northern Ireland, United Kingdom",
+        "center": [
+          -7.34167,
+          54.99167
+        ],
+      }
+    ]
+  }]
+}
