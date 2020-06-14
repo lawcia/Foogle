7953d4ad35bd95d286cdd112f73a8aa78acb0ad2
lawcia <lawrencianjume@gmail.com>
2020-06-14 22:42:34 +0100

    reverse geocode api

--- project/frontend/src/api/mapBoxApi.js
+++ project/frontend/src/api/mapBoxApi.js
@@ -7,7 +7,18 @@ export function convertSearchLocationToCoordinates(searchLocation) {
   return axios.get(
     `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchLocation}.json?access_token=${token}`
   ).then(response => {
-    if (response.status === 200)  return response.data.features;
+    if (response.status === 200) return response.data.features;
+    throw new Error();
+  }).catch(error => {
+    throw error;
+  })
+}
+
+export function convertCoordinatesToSearchLocation(longitude, latitude) {
+  return axios.get(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${token}`
+  ).then(response => {
+    if (response.status === 200) return response.data.features[0].place_name;
     throw new Error();
   }).catch(error => {
     throw error;
