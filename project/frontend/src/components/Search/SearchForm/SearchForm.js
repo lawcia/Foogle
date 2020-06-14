172903e5541b96798f1f59ad92e828eeee7e1bfa
lawcia <lawrencianjume@gmail.com>
2020-06-14 22:36:53 +0100

    dispatch get current position

--- project/frontend/src/components/Search/SearchForm/SearchForm.js
+++ project/frontend/src/components/Search/SearchForm/SearchForm.js
@@ -3,11 +3,11 @@ import SearchLocationInput from "../SearchLocationInput/SearchLocationInput";
 import SearchKeywordInput from "../SearchKeywordInput/SearchKeywordInput";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { loadFeatures } from "../../../redux/actions/searchActions";
+import { loadFeatures, getCurrentPosition } from "../../../redux/actions/searchActions";
 import Button from "../../Buttons/Button";
 import "./SearchForm.css";
 
-export const SearchForm = ({ features, loadFeatures }) => {
+export const SearchForm = ({ features, loadFeatures, getCurrentPosition }) => {
   const [searchLocation, setSearchLocation] = useState("");
   const [searchKeyword, setSearchKeyword] = useState("");
   const [showDropdown, setShowDropdown] = useState(false);
@@ -20,9 +20,7 @@ export const SearchForm = ({ features, loadFeatures }) => {
     const { name, value } = event.target;
     if (name === "searchLocation") {
       setSearchLocation(value);
-      loadFeatures(searchLocation)
-        .then(() => {})
-        .catch((error) => console.log(error));
+      loadFeatures(searchLocation).catch((error) => console.log(error));
     } else if (name === "searchKeyword") {
       setSearchKeyword(value);
     }
@@ -30,6 +28,11 @@ export const SearchForm = ({ features, loadFeatures }) => {
 
   const handleClick = (event, location = "__NEAR_ME__") => {
     if (location === "__NEAR_ME__") {
+      setSearchLocation("");
+      setCoordinates({longitude: null, latitude: null})
+      getCurrentPosition().then(() => {
+        
+      }).catch((error) => console.log(error));
     } else {
       const { placeName, longitude, latitude } = location;
       setSearchLocation(placeName);
@@ -70,6 +73,7 @@ export const SearchForm = ({ features, loadFeatures }) => {
 SearchForm.protoTypes = {
   features: PropTypes.array.isRequired,
   loadFeatures: PropTypes.func.isRequired,
+  getCurrentPosition: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => {
@@ -80,6 +84,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = {
   loadFeatures,
+  getCurrentPosition,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchForm);
