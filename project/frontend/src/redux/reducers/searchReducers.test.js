7fc4eb82051b689fbe0ff5babbfc75a40369819b
lawcia <lawrencianjume@gmail.com>
2020-06-14 23:01:06 +0100

    reducer test for get current position success

--- project/frontend/src/redux/reducers/searchReducers.test.js
+++ project/frontend/src/redux/reducers/searchReducers.test.js
@@ -1,19 +1,39 @@
 import searchReducer from "./searchReducer";
 import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
-import { data } from "../../mockAPIs/mapbox";
+import {
+  data
+} from "../../mockAPIs/mapbox";
 
 describe("search reducer", () => {
   it("should return initial state", () => {
     expect(searchReducer(undefined, {}))
-    .toEqual(initialState)
+      .toEqual(initialState)
   })
 
   it("should return state with features data", () => {
-    expect(searchReducer(undefined, 
-      { type: types.LOAD_FEATURES_SUCCESS, 
-        features: data.features }
-        ))
-    .toEqual({...initialState, features: data.features})
+    expect(searchReducer(undefined, {
+        type: types.LOAD_FEATURES_SUCCESS,
+        features: data.features
+      }))
+      .toEqual({
+        ...initialState,
+        features: data.features
+      })
+  })
+
+  it("should return state with coordinates", () => {
+    const coords = {
+      longitude: 60,
+      latitude: 80
+    }
+    expect(searchReducer(undefined, {
+      type: types.GET_CURRENT_POSITION_SUCCESS,
+      coords
+    })).toEqual({
+      ...initialState,
+      longitude: coords.longitude,
+      latitude: coords.latitude
+    })
   })
 })
