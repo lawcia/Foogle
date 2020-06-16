17cdcca95f440258117fbea4869dc5b932364bb3
lawcia <lawrencianjume@gmail.com>
2020-06-16 13:27:09 +0100

    style login component

--- project/frontend/src/components/Login/Login.js
+++ project/frontend/src/components/Login/Login.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import Button from "../Buttons/Button";
+import logo from "../../images/foogle.png";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
-const Login = () => {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
-  const [ error, setError ] = useState('');
+const Login = ({error=""}) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -13,29 +14,31 @@ const Login = () => {
 
   return (
     <div className="Login">
+      <img className="Login__logo" src={logo} alt="foogle logo" />
+      <h3 className="Login__heading">Welcome back</h3>
+      {error.length > 0 && <div className="Login--error"><p>{error}</p></div>}
       <form className="Login__form" onSubmit={handleSubmit}>
-        <h3>Login</h3>
+        <label className="Login__label" htmlFor="username">Username</label>
         <input
-          className="input input-m"
+          className="input input-m Login__input"
           onChange={(e) => setUsername(e.target.value)}
           type="text"
           name="username"
-          placeholder="Enter your username"
           value={username}
         />
+        <label className="Login__label" htmlFor="password">Password</label>
         <input
-          className="input input-m"
+          className="input input-m Login__input"
           onChange={(e) => setPassword(e.target.value)}
           type="password"
           name="password"
-          placeholder="Enter your password"
           value={password}
         />
-        {error && (
-          <p className="align-text error-msg">{error}</p>
-        )}
-        <Button value="Log in" colour="blue"/>
+        <Button value="Log in" colour="blue" />
       </form>
+      <div>
+        <p>New to foogle? <Link to="/signup">Create an account</Link></p>
+      </div>
     </div>
   );
 };
