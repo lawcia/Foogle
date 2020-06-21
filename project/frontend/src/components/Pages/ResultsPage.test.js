5818243d7b897ed1508a6c1b8aab634a01710f4f
lawcia <lawrencianjume@gmail.com>
2020-06-21 20:57:13 +0100

    write test to get list of results

--- project/frontend/src/components/Pages/ResultsPage.test.js
+++ project/frontend/src/components/Pages/ResultsPage.test.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ResultsPage from './ResultsPage';
 import { shallow } from 'enzyme';
 import axios from 'axios';
+import { data } from '../../mockAPIs/zomato';
 
 jest.mock('axios');
 
 describe('<ResultsPage />', () => {
   it('should render a list of restaurants', () => {
-    axios.get.mock
+    axios.get.mockResolvedValueOnce({ data })
     const wrapper = shallow(<ResultsPage />);
-    expect(wrapper.find('.Restaurant').length).toEqual(10);
+    expect(wrapper.find('.Restaurant').length).toEqual(20);
   })
 });
