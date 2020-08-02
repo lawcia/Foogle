28ba4d3dc19584a846cd6849876b774d128488e5
lawcia <lawrencianjume@gmail.com>
2020-08-03 00:33:47 +0100

    redirect to favourites page after login

--- project/frontend/src/components/Pages/SignupPage.js
+++ project/frontend/src/components/Pages/SignupPage.js
@@ -3,13 +3,13 @@ import SignUp from "../Authentication/Signup/Signup";
 import logo from "../../images/foogle.png";
 import picture from "../../images/female-friends-hanging-out-cafe.jpg";
 import "./SignupPage.css";
-import { signupUser } from "../../redux/actions/authActions";
+import { signupUser, loginUser } from "../../redux/actions/authActions";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { toast } from "react-toastify";
 
-export const SignupPage = ({ signupUser, error: signupError }) => {
+export const SignupPage = ({ signupUser, error: signupError, createdUser, isAuthenticated, loginUser }) => {
   const [user, setUser] = useState({
     username: "",
     password: "",
@@ -31,6 +31,21 @@ export const SignupPage = ({ signupUser, error: signupError }) => {
     setError({ username, email, password });
   }, [signupError]);
 
+  useEffect(() => {
+    if(createdUser){
+      loginUser({ username: user.username, password: user.password })
+      toast(`✨ ${user.username}, Welcome to foogle!`, {
+        autoClose: 15000,
+      });
+    }
+  }, [createdUser])
+
+  useEffect(() => {
+    if(isAuthenticated){
+      history.push("/favourites")
+    }
+  }, [isAuthenticated])
+
   const handleChange = (event) => {
     setUser({ ...user, [event.target.name]: event.target.value });
   };
@@ -40,12 +55,7 @@ export const SignupPage = ({ signupUser, error: signupError }) => {
     if (user.password !== user.password2) {
       setError({ ...error, password: "Your passwords do not match" });
     } else {
-      signupUser(user).then(() => {
-          toast("✨ You've joined foogle. You can now login.", {
-            autoClose: 15000,
-          });
-          history.push("/login");
-      });
+      signupUser(user)
     }
   };
 
@@ -79,17 +89,23 @@ export const SignupPage = ({ signupUser, error: signupError }) => {
 
 SignupPage.propType = {
   signupUser: PropTypes.func.isRequired,
-  error: PropTypes.object,
+  loginUser: PropTypes.func.isRequired,
+  error: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]).isRequired,
+  createdUser: PropTypes.bool.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = (state) => {
   return {
     error: state.authReducer.signupError,
+    createdUser: state.authReducer.createdUser,
+    isAuthenticated: state.authReducer.isAuthenticated
   };
 };
 
 const mapDispatchToProps = {
   signupUser,
+  loginUser
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignupPage);
